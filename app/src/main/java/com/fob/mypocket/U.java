2bdc9dc6c6cecfd4739c7cc2c15ead1718daef06
Fernando <ofernandobarbosa@gmail.com>
2023-02-03 18:01:39 -0300

    improvements

--- app/src/main/java/com/fob/mypocket/U.java
+++ app/src/main/java/com/fob/mypocket/U.java
@@ -2,5 +2,5 @@ package com.fob.mypocket;
 
 public class U {
 
-    public static final String BASE_URL = "https://1bef-177-22-173-42.sa.ngrok.io/consulta";
+    public static final String BASE_URL = "https://f1e8-177-22-173-42.sa.ngrok.io/consulta";
 }
